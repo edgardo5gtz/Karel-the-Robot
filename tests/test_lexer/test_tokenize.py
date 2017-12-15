9bb43a164573f3cab90860a2d19d56d5b8ddb18c
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2017-12-15 01:26:12 -0600

    Test tokenize funciton.

--- tests/test_lexer/test_tokenize.py
+++ tests/test_lexer/test_tokenize.py
@@ -1,9 +1,23 @@
 from unittest import TestCase
+from sample.lexer import lexer as lex
 
 
 class TestLexer(TestCase):
     def setUp(self):
-        pass
+        self.code_input = open("test_lexer_input.txt", 'r')
+        self.assertion_output = open("test_lexer_assertions.txt", 'r')
+        self.lexer_output = open("test_lexer_output.txt", 'w')
 
     def test_tokenize(self):
-        self.assertListEqual()
+        list_of_tokens = lex.tokenize(self.code_input.read())
+        output = list()
+        for token in list_of_tokens:
+            output.append(str(token))
+        answer = self.assertion_output.read().splitlines()
+        self.close_files()
+        self.assertListEqual(output, answer)
+
+    def close_files(self):
+        self.code_input.close()
+        self.assertion_output.close()
+        self.lexer_output.close()
