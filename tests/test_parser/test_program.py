0209007c1c4c1fc369d2a54eaaa8088b8538bbaf
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2017-12-26 20:35:09 -0600

    Make a more test cases for the program function.

--- tests/test_parser/test_program.py
+++ tests/test_parser/test_program.py
@@ -5,16 +5,43 @@ from sample.lexer.lexer import tokenize
 
 class TestProgram(TestCase):
     def setUp(self):
-        self.code_input = open("test_parser_input.txt", "r")
-        self.list_of_tokens = list()
-        for token in tokenize(self.code_input.read()):
-            self.list_of_tokens.append(token)
+        self.assert_succesful_parsing = "class program { }"   # Successful parsing
+        self.assert_raise_class_except = "program {}"         # Raise class exception
+        self.assert_raise_program_except = "class {}"         # Raise program exception
+        self.assert_raise_lcurl_except = "class program }"    # Raise lcurl exception
+        self.assert_raise_rcurl_except = "class program {"    # Raise rcurl exception
 
-    def test_program(self):
-        parser.tokens = self.list_of_tokens
-        result = parser.parse()
-        self.close_files()
-        self.assertTrue(result)
+    def test_program_successful_parsing(self):
+        parser.index = 0
+        parser.tokens = TestProgram.set_list_of_tokens(self.assert_succesful_parsing)
+        try:
+            parser.parse()
+        except Exception as error:
+            self.fail("Unexpected failure as {}".format(error))
 
-    def close_files(self):
-        self.code_input.close()
+    def test_program_raise_class_exception(self):
+        parser.index = 0
+        parser.tokens = TestProgram.set_list_of_tokens(self.assert_raise_class_except)
+        self.assertRaises(RuntimeError, parser.parse)
+
+    def test_program_raise_program_exception(self):
+        parser.index = 0
+        parser.tokens = TestProgram.set_list_of_tokens(self.assert_raise_program_except)
+        self.assertRaises(RuntimeError, parser.parse)
+
+    def test_program_raise_lcurl_exception(self):
+        parser.index = 0
+        parser.tokens = TestProgram.set_list_of_tokens(self.assert_raise_lcurl_except)
+        self.assertRaises(RuntimeError, parser.parse)
+
+    def test_program_raise_rcurl_exception(self):
+        parser.index = 0
+        parser.tokens = TestProgram.set_list_of_tokens(self.assert_raise_rcurl_except)
+        self.assertRaises(RuntimeError, parser.parse)
+
+    @staticmethod
+    def set_list_of_tokens(code):
+        list_of_tokens = list()
+        for token in tokenize(code):
+            list_of_tokens.append(token)
+        return list_of_tokens
