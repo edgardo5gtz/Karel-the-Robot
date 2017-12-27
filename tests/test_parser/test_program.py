0aa272b566cb026512f95f9cf818565d8628920e
Edgardo Gutierrez <edgardo5gtz@gmail.com>
2017-12-26 18:43:19 -0600

    Test changes in the parser script not affect the function.
    The test case pass as for this changes.

--- tests/test_parser/test_program.py
+++ tests/test_parser/test_program.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 from sample.parser import parser
 from sample.lexer.lexer import tokenize
 
+
 class TestProgram(TestCase):
     def setUp(self):
         self.code_input = open("test_parser_input.txt", "r")
@@ -10,7 +11,8 @@ class TestProgram(TestCase):
             self.list_of_tokens.append(token)
 
     def test_program(self):
-        result = parser.parse(self.list_of_tokens)
+        parser.tokens = self.list_of_tokens
+        result = parser.parse()
         self.close_files()
         self.assertTrue(result)
 
